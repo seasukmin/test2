7d9570d85c98421ebc43fd0e10fd1db1528f5857
seasukmin <167054580+seasukmin@users.noreply.github.com>
2024-05-31 11:03:01 +0900

    1

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -3,24 +3,45 @@ import {
   getFirestore,
   getDocs,
   collection,
-} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+  setDoc,
+  doc,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCREEdi1EUrARen8jLYiZXP1o7mOkdj1lc",
-  authDomain: "myproject-8e5f0.firebaseapp.com",
+  authDomain: "myproject-8e5f0.firebaseapp.com",j
   projectId: "myproject-8e5f0",
   storageBucket: "myproject-8e5f0.appspot.com",
   messagingSenderId: "981961164823",
   appId: "1:981961164823:web:227d68716308f588fbf452",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function getMembers() {
-  const collect = await collection(db, "member");
+async function getDatas(collectionName) {
+  const collect = await collection(db, collectionName);
   const snapshot = await getDocs(collect);
-  return snapshot;
 
-  // console.log(snapshot);
+  return snapshot;
 }
-export { db, getMembers };
+
+async function addDatas(collectionName, dataObj) {
+  try {
+    // 문서 ID 수동
+    // const saveDoc = await doc(db, collectionName, '3');
+    // console.log(`doc() 결과: ${saveDoc}`);
+    // const saveResult = await setDoc(saveDoc, dataObj);
+    // console.log(`setDoc() 결과: ${saveResult}`);
+
+    // 문서 ID 자동
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+export { db, getDatas, addDatas };
