bf5d850dc605881a70906a8d921cbdbd96734281
seasukmin <167054580+seasukmin@users.noreply.github.com>
2024-06-03 17:35:49 +0900

    1

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -6,6 +6,9 @@ import {
   setDoc,
   doc,
   addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -36,11 +39,25 @@ async function addDatas(collectionName, dataObj) {
 
     // 문서 ID 자동
     const collect = await collection(db, collectionName);
-    await addDoc(collect, dataObj);
+    await addDoc(collect, dataObj); // 결과 == undefined
     return true;
   } catch (error) {
     return false;
   }
 }
 
-export { db, getDatas, addDatas };
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  const docRef = await doc(db, collectionName, docId);
+  const docData = await getDoc(docRef);
+  await updateDoc(docRef, updateInfoObj);
+  // doc(db, 컬렉션명, 문서ID);
+  // getDocs(문서레퍼런스);
+  // updateDoc(문서데이터, 수정할 정보객체);
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
