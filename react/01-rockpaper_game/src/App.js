2e108b8fe4f4583fb4f00f696126b8e3a9332d55
mj <seh9210@gmail.com>
2024-07-01 17:15:14 +0900

    d

--- react/01-rockpaper_game/src/App.js
+++ react/01-rockpaper_game/src/App.js
@@ -1,18 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import resetButton from "./assets/ic-reset.svg";
 
 function App() {
   return (
-    <div>
+    <div className="Container">
+      <div className="nav">
+        <h1>가위바위보</h1>
+        <button>
+          <img src={resetButton} />
+        </button>
+      </div>
       <div>
+        <div></div>
         <div></div>
         <div></div>
       </div>
       <div>
         <img />
+        <div></div>
+        <img />
       </div>
+      <div></div>
+      <div></div>
       <div>
+        <button>주먹</button>
         <button>가위</button>
+        <button>보</button>
       </div>
     </div>
   );
