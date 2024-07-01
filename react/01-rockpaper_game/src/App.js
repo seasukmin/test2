02d53a60bf9bdb9d9f23e2759151ef2e6938ea14
seasukmin <167054580+seasukmin@users.noreply.github.com>
2024-07-01 23:29:58 +0900

    1

--- react/01-rockpaper_game/src/App.js
+++ react/01-rockpaper_game/src/App.js
@@ -1,44 +1,49 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import resetButton from "./assets/ic-reset.svg";
 import rockButton from "./assets/rock.svg";
 import sisserButton from "./assets/scissor.svg";
 import paperButton from "./assets/paper.svg";
+import Board from "./Board";
 
+const [Mynum, setMynum] = useState(1);
 function App() {
+  function RockScissorPaper() {}
+
   return (
     <div className="Container">
       <div className="nav">
         <h1>가위바위보</h1>
-        <button>
+        <button class="buttonClass">
           <img src={resetButton} />
         </button>
       </div>
       <div className="Point">
-        <div>나</div>
+        <Board point="g" />
         <div>:</div>
-        <div>상대</div>
+        <Board point="k" />
       </div>
       <div className="Main">
         <img src={rockButton} />
         <div>vs</div>
         <img src={rockButton} />
       </div>
-      <div>
+      <div className="xnumber">
         배점
         <input type="text" />매
       </div>
-      <div>승부기록</div>
-      <div>
-        <button>
+      <div className="whowin">승부기록</div>
+      <div className="footer">
+        <button class="rock" onClick={RockScissorPaper}>
           <img src={rockButton} />
           주먹
         </button>
-        <button>
+        <button class="scissor">
           <img src={sisserButton} />
           가위
         </button>
-        <button>
+        <button class="paper">
           <img src={paperButton} />보
         </button>
       </div>
---
+++ react/01-rockpaper_game/src/Board.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Board({ point }) {
+  return <div>gdgd</div>;
+}
+
+export default Board;
