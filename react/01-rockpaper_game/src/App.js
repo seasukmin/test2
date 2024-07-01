c6ad5a40173a49efaece98b37346067b17c10a01
mj <seh9210@gmail.com>
2024-07-01 16:51:16 +0900

    240701 -4

---
+++ react/01-rockpaper_game/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <img />
+      </div>
+      <div>
+        <button>가위</button>
+      </div>
+    </div>
+  );
+}
+export default App;
