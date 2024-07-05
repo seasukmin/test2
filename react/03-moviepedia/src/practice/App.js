6b130e2509026dc9dd38a9965aa40eda7de05de3
mj <seh9210@gmail.com>
2024-07-05 17:22:21 +0900

    1

--- react/03-moviepedia/src/practice/App.js
+++ react/03-moviepedia/src/practice/App.js
@@ -1,7 +1,29 @@
-import React from "react";
-import State from "./state";
-function App(props) {
-  return <State />;
+import { useEffect, useState } from "react";
+import "./App.css";
+import ReviewForm from "./ReviewForm";
+import ReviewList from "./ReviewList";
+import logoImg from "./assets/logo.png";
+import mockItems from "./mock.json";
+import {
+  addDatas,
+  deleteDatas,
+  getDatas,
+  getDatasByOrder,
+  getDatasByOrderLimit,
+} from "./firebase";
+
+const LIMIT = 10;
+
+function AppSortButton({ children, onClick, selected }) {
+  let isSelected = "";
+  if (selected) {
+    isSelected = "selected";
+  }
+  return {
+    // <button className={`AppSortButton ${isSelected}`} onClick={onClick}>
+    // {children}
+    // </button>
+  };
 }
 
-export default App;
+function App() {}
